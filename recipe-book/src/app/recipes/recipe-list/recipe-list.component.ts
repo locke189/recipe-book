3b0bd9e0dafe3f014fbec726497cd4650fa030ff
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2016-11-06 07:43:38 -0500

    Lecture 52 - Ingredients List with *ngFor

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,8 +6,11 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
-  recipe = new Recipe('Dummy', 'Dummy', 'http://prevenblog.com/wp-content/uploads/Dummy.jpg');
+  recipes: Recipe[] = [
+    new Recipe('Scnitzel', 'Yummy', 'http://kitchenproject.com/german/recipes/Schnitzel/schnitzel.jpg',[]),
+    new Recipe('Summer Salad', 'Okayish', 'http://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/beetroot-feta-grain-salad.jpg',[])
+  ];
+  recipe = new Recipe('Dummy', 'Dummy', 'http://prevenblog.com/wp-content/uploads/Dummy.jpg',[]);
   @Output() recipeSelected = new EventEmitter();
 
   constructor() { }
